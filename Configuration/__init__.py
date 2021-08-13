b63ab1bdf7f173e255c71d7bbaf17b6940b1d03d
ElvenEars <34861658+ElvenEars@users.noreply.github.com>
2021-08-13 15:24:11 +0300

    fix network bug + fix sound

--- Configuration/__init__.py
+++
@@ -1,8 +0,0 @@
-
-class Configuration:
-
-    def __init__(self):
-        self.server_ip = "10.21.10.125"
-        self.server_sip_id = "16775904"
-        self.server_port = str(19888)
-        self.server_voice_port = str(30000)
--- Log/__init__.py
+++
@@ -1,7 +0,0 @@
-from _datetime import datetime
-
-
-class Log(object):
-
-    def to_log(self, data):
-        print("Message: " + data)
